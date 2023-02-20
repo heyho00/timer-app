67533571f3686c498fa2ba6b3728da4b3a9e43df
heyho00 <heyho9292@gmail.com>
2023-02-21 00:25:35 +0900

    ➕ ref 값 변경 실습

--- src/components/TimerControl.tsx
+++ src/components/TimerControl.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Timer() {
     useEffect(()=>{
@@ -19,30 +19,39 @@ function Timer() {
 }
 
 export default function TimerControl() {
-    const ref = {
-        value: 1,
-    }
-    ref.value = 2
+    // const ref = {
+    //     value: 1,
+    // }
+    // ref.value = 2
     // 이런 ref를 만들고
     // const로 만든 ref 지만 ref.value의 값은 변경 가능하다. 이런 개념.
     // 각자 다른 컴포넌트들이 자신들만의 이런 객체를 갖고싶어 -> useRef
 
+    const ref = useRef(1)
+    // useState는 상태가 변경되면 해당 컴포넌트와 하위 컴포넌트를 다시 렌더링함.
+    // useRef는 ref.current 값이 변해도 리렌더 안함. 
+    // 다른 이유로 렌더될때만 반영됨.
+
     const [playing, setPlaying] = useState(false)
 
     const handleClick = () => {
-        setPlaying(!playing)
+        ref.current += 1
+        // setPlaying(!playing)
     }
-
+console.log(ref.current,'ss')
     return (
         <div>
-            {playing ?(
+            {/* {playing ?(
                 <Timer />
             ): (
                 <p>Stop</p>
-            ) }
+            ) } */}
             <button type="button" onClick={handleClick}>
                 Toggle
             </button>
+            <button type="button" onClick={()=>console.log(ref.current)}>see ref</button>
+
+            <p>{ref.current}</p>
         </div>
     )
 }
