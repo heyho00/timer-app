e9dbceb89c6ca0643684e6b17e31b4a31dc2e864
heyho00 <heyho9292@gmail.com>
2023-02-20 22:52:28 +0900

    ➕ dependency array를 이용한 data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TimerControl from "./components/TimerControl";
 
 export default function App() {
 	const [input, setInput] = useState('')
+	const [products, setProducts] = useState([])
 
 	// 1.
     // useEffect(()=>{
@@ -51,6 +52,24 @@ export default function App() {
 	// }
 	// useEffect 안의 return 함수  는 렌더가 끝날때 useEffect를 종료처리 해준다.? 
 	// 렌더가 끝날때 실행되는 함수이다? 맞는 표현인가.
+
+	//5. 이런식으로 dependency array 이용함.
+	// fetch 처음 한번만 받고싶을때 빈배열로.
+	// state 변화에 영향을 받는 , 리렌더 하고싶은 의존성이 있으면 배열에 넣는다.
+	// 	useEffect(() => {
+	// 	const fetchProducts = async () => {
+	// 		const url = 'http://localhost:3000/products';
+	// 		const response = await fetch(url);
+	// 		const data = await response.json();
+	// 		setProducts(data.products);
+	// 	};
+	// 	fetchProducts();
+	// }, []);
+	// 안에 함수를 밖으로 빼도된다. fetchProducts를. 그러나
+	// useEffect 클로저라서 안에서 참조하는 다른 state가 있으면 그걸 처음에 바인딩해서
+	// 사용하기 때문에 문제가 될 수 있다.
+	// https://overreacted.io/ko/a-complete-guide-to-useeffect/#%ED%95%A8%EC%88%98%EB%A5%BC-%EC%9D%B4%ED%8E%99%ED%8A%B8-%EC%95%88%EC%9C%BC%EB%A1%9C-%EC%98%AE%EA%B8%B0%EA%B8%B0
+	// 가능하면 다 안쪽으로 넣으라는 말.
 	
   return (
 		<>
