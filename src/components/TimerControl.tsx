4061a5a20215341c25f8e4a4d63f6d4169ff838d
heyho00 <heyho9292@gmail.com>
2023-02-21 00:03:13 +0900

    ➕ ref 객체

--- src/components/TimerControl.tsx
+++ src/components/TimerControl.tsx
@@ -19,6 +19,14 @@ function Timer() {
 }
 
 export default function TimerControl() {
+    const ref = {
+        value: 1,
+    }
+    ref.value = 2
+    // 이런 ref를 만들고
+    // const로 만든 ref 지만 ref.value의 값은 변경 가능하다. 이런 개념.
+    // 각자 다른 컴포넌트들이 자신들만의 이런 객체를 갖고싶어 -> useRef
+
     const [playing, setPlaying] = useState(false)
 
     const handleClick = () => {
