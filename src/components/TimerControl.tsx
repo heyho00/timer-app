f9d7420ec50c42a7fbac431832688fa339841000
heyho00 <heyho9292@gmail.com>
2023-02-20 22:27:14 +0900

    ➕ title name 저장, cleanup 함수로 원복

--- src/components/TimerControl.tsx
+++ src/components/TimerControl.tsx
@@ -2,15 +2,19 @@ import { useEffect, useState } from "react";
 
 function Timer() {
     useEffect(()=>{
+        const savedTitle = document.title;
+
 		const id = setInterval(()=>{
 			document.title = `Now: ${new Date().getTime()}`;
 		},2000)
 
         return () => {
-        console.log('end of effect')
         clearInterval(id)
+        document.title = savedTitle // 저장해둔 title로 원복해준다.
+        // 결론적으로 useEffect의 return은 끝날때에 대한 처리이다.
     }
 	})
+
     return <p>Playing</p>
 }
 
